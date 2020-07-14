a823c894b0016938fba70a1e0c7950d9030f59d8
IreneZoi <irene.zoi@cern.ch>
2020-07-14 12:16:45 +0200

    analysed last measurements

--- Macros/resolution_2773_TreeEntries.C
+++ Macros/resolution_2773_TreeEntries.C
@@ -19,20 +19,21 @@ bool print=true;
 using namespace std;
 TString outputDir = "/home/zoiirene/Output/Plots/";
   
-void resolution_2773_TreeEntries(TString function = "RMS95")
+void resolution_2773_TreeEntries(TString function = "RMSself")
 {
 
   TString detectorA[Angles]={"148"};
   TString detectorB[Angles]={"146"};
   TString detectorC[Angles]={"163"};
-  TString labelA[Angles]={"Pstop_RD53Apads_FDB, thr 12 ADC"};
-  TString labelB[Angles]={"Pspray_default_FDB, thr 26 ADC"};
-  TString labelC[Angles]={"Pspray_RD53Apads_FDB, thr 12 ADC"};
+  TString labelA[Angles]={"Pstop_RD53Apads_FDB, thr 11 ADC"};
+  TString labelB[Angles]={"Pspray_default_FDB, thr 13 ADC"};
+  TString labelC[Angles]={"Pspray_RD53Apads_FDB, thr 14 ADC"};
   TString info = "beam energy 5.6 GeV";
   
   
   //  Double_t Angle[Angles]={-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,9.5,10,10.5,11,11.5,12,13,14,15,16,17,18,19,20,21,22,23,24};
   Double_t Angle[Angles]={15};
+  Double_t Percentage[Angles];
   Double_t AngleError[Angles];
   Double_t TanAngle[Angles];
   Double_t TanAngleError[Angles];
@@ -41,7 +42,7 @@ void resolution_2773_TreeEntries(TString function = "RMS95")
   Double_t NcolError[Angles];
   Double_t ResolutionError[Angles];
   TString ss_Angle[Angles];
-  
+  TString name = "closest_A11C14";
   TH1F * h_res;//[Angles];
 
   Int_t run[Angles]={2773}; 
@@ -50,12 +51,12 @@ void resolution_2773_TreeEntries(TString function = "RMS95")
 
   float angleunit = 1.25;
   if(print) cout << "Getting files "  << endl;
-  TString pitch[Angles];
+  TString pitch = "25";
 
 
   for(int i=0; i<Angles; i++)
     {
-      pitch[i] = "25";
+
       if(print) cout << "Run: "<< i <<" " << run[i] << endl;
       Run[i].Form("%d",run[i]);
       if(print) cout << "Run: " << Run[i] << endl;
@@ -73,16 +74,16 @@ void resolution_2773_TreeEntries(TString function = "RMS95")
  
   MapTH1 res_map;
   std::map<std::pair<TString, TString>, TH1F *>::iterator it;
-  int i_dphcut[dphcuts] = {26};
-  TString ss_dphcut[dphcuts] = {"26"};
+  int i_dphcut[dphcuts] = {13};
+  TString ss_dphcut[dphcuts] = {"13"};
 
   TString Label[comparisons] = {"straightTracksY_isoAandCandB_straightTracksX"};
-  TString Hist = "dx3_clchargeABC90evR95"; 
+  TString Hist = "dx3_clchargeABC90evR"; 
   bool dphcut = true;
 
   //GetHists(&res_map,Angles, dphcuts, comparisons,dphcut, Run, i_dphcut, Label, Hist, h_res);
   TH1F * hdx3_clchargeABC90evR; // = new TH1I("dx3_clchargeABC90evR ", "triplet dx_clchargeABC90evR ; dx [mm];triplets", 500, -0.5, 0.5 ); //Cut at 90% events in Landau (only high tail)
-  hdx3_clchargeABC90evR =    GetCheckHists(&res_map, Angles, dphcuts,dphcut, Run,ss_dphcut,pitch, Hist,hdx3_clchargeABC90evR,true,"testEntries");
+  res_map =    GetCheckHists(&res_map, Angles, dphcuts,dphcut, Run,ss_dphcut,pitch, Hist,hdx3_clchargeABC90evR,true,name);
   
   for ( it = res_map.begin(); it != res_map.end(); it++ )	{
     cout << it->first.first << " " << it->first.second << " " <<  it->second->GetEntries() << endl;
@@ -95,7 +96,7 @@ void resolution_2773_TreeEntries(TString function = "RMS95")
   ofstream myfile[Angles];
   for(int i=0; i<Angles; i++){
 
-      myfile[i].open ("/home/zoiirene/Output/TextFiles/Ascan_rmsTree_"+detectorB[i]+"_dphcutB"+ss_dphcut[i]+"_"+function+"_testEntries.txt");
+      myfile[i].open ("/home/zoiirene/Output/TextFiles/Ascan_rmsTree_"+detectorB[i]+"_dphcutB"+ss_dphcut[i]+"_"+function+"_"+name+".txt");
       myfile[i] << "A " << detectorA[i] << "\n";
       myfile[i] << labelA[i] << "\n";
       myfile[i] << "B " << detectorB[i]<< "\n";
@@ -105,14 +106,14 @@ void resolution_2773_TreeEntries(TString function = "RMS95")
       myfile[i] << info << " " << Label[0] << "\n";
       myfile[i] << "Angle RMS95(um) Error\n";
 
-      auto it2 = res_map.find(std::make_pair(Run[i]+"_"+ss_dphcut[i]+"_"+pitch[0],Hist));
+      auto it2 = res_map.find(std::make_pair(Run[i]+"_"+ss_dphcut[i]+"_"+pitch,Hist));
       if(it2  != res_map.end())
 	{
 	  if(print)               cout << " found map " << endl;
 	  if(print)                   cout << "map key " << it2->first.first << " " << it2->first.second << " " << it2->second->GetEntries() << endl;
 
 	  
-	  FitTH1(it2->second, &(Resolution[i]), &(ResolutionError[i]), ss_Angle[i], detectorA[i], detectorB[i], detectorC[i], function );
+	  FitTH1(it2->second, &(Resolution[i]), &(ResolutionError[i]), ss_Angle[i], detectorA[i], detectorB[i], detectorC[i], function, &(Percentage[i]) );
 	  if(print) cout << "Angle " << i<< ": " << ss_Angle[i] << " degrees -> Resolution: " << Resolution[i] << " and res err: " << ResolutionError[i] << endl;
 	  myfile[i] << " " << Angle[i] << " " << Resolution[i] << " " << ResolutionError[i] << "\n";
 	}
@@ -131,7 +132,7 @@ void resolution_2773_TreeEntries(TString function = "RMS95")
   ofstream myfile2[Angles];
   for(int i=0; i<Angles; i++){
   
-  myfile2[i].open ("/home/zoiirene/Output/TextFiles/Ascan_resTree_"+detectorB[i]+"_dphcutB"+ss_dphcut[i]+"_"+function+"_testEntries.txt");
+  myfile2[i].open ("/home/zoiirene/Output/TextFiles/Ascan_resTree_"+detectorB[i]+"_dphcutB"+ss_dphcut[i]+"_"+function+"_"+name+".txt");
   myfile2[i] << "A " << detectorA[i] << "\n";
   myfile2[i] << labelA[i] << "\n";
   myfile2[i] << "B " << detectorB[i]<< "\n";
