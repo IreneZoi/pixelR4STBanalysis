1ad3178926e7d40643f216281ea09fb493dc8d3a
IreneZoi <irene.zoi@cern.ch>
2020-11-01 19:26:30 +0100

    improved legend and labels

--- Macros/resolutionLiteratureSummary.C
+++ Macros/resolutionLiteratureSummary.C
@@ -23,6 +23,43 @@ void resolutionLiteratureSummary()
   Double_t ResolutionError[measurements];
   Double_t Pitch[measurements];
   TString Legend[measurements];
+  TString Label[measurements] ={
+    "n-in-n, 285 #mum, psi46dig",
+    "n-in-n, 285 #mum, psi46dig",
+    "n-in-n, 285 #mum, psi46dig",
+    "n-in-p, 200 #mum, timepix3",
+    "n-in-p, 150 #mum, RD53A (CMS)",
+    "n-in-n, 280 #mum, FE-A/FE-B",
+    "3D, n-in-p, 130 #mum, RD53A (CMS)",
+    "n-in-n, 280 #mum, FE-A/FE-B",
+    "n-in-n, 280 #mum, FE-A/FE-B",
+    "n-in-n, 280 #mum, FE-A/FE-B",
+    "n-in-n, 280 #mum, FE-A/FE-B",
+    "n-in-p, 100 #mum, RD53A (ATLAS)",
+    "n-in-p, 150 #mum, R4S",
+    "n-in-n, 285 #mum, psi46dig",
+    "ATLASpix Simple, 50 #mum",
+    "DEPFET, 450 #mum",
+    "SOI, 500 #mum",
+    "High-Resistivity CMOS, 25 #mum",
+    "High-Voltage CMOS C3PD, 50 #mum",
+    "n-in-n, 285 #mum, psi46dig",
+    "n-in-p, 100 #mum, RD53A (ATLAS)",
+    "3D, n-in-p, 130 #mum, RD53A (CMS)",
+    "n-in-p, 150 #mum, RD53A (CMS)",
+    "n-in-p, 150 #mum, R4S",
+    "CLICpix2 prototype, 130 #mum",
+    "DEPFET, 450 #mum",
+    "n in p, High Voltage CMOS, 15 #mum",
+    "SOI 0.2 #mum, 200 #mum",
+    "DEPFET, 450 #mum",
+    "Mimosa26 sensors, 50 #mum",
+    "Mimosa18 sensors, 14 #mum",
+    "FPIX SOI 0.2 #mum, 400 #mum"
+  };
+
+
+    
   // change color and markers: full = normal sensors, empty = cmos
   // colors dark = 120 GeV, medium = ~ GeV, light = unknown
   int colors[measurements];
@@ -59,7 +96,7 @@ void resolutionLiteratureSummary()
 
   
 
-  TCanvas *cFDB2 = new TCanvas("cFDB2", "FDB resolution", 1500, 900);
+  TCanvas *cFDB2 = new TCanvas("cFDB2", "FDB resolution", 600, 600);
   gPad->SetTicks(1,1);
   gROOT->SetStyle("Plain");
   gStyle->SetPadGridX(0);
@@ -67,11 +104,25 @@ void resolutionLiteratureSummary()
   gStyle->SetPalette(1);
   gStyle->SetOptStat(0);
   gStyle->SetOptTitle(0);
-
-  TLegend* legFDB2 = new TLegend(0.15,0.4,0.5,0.9);
+  gStyle->SetTextFont(43);
+  gStyle->SetTextSize(10);
+  gStyle->SetLegendFont(43);
+  gStyle->SetLegendTextSize(15);
+  
+  TLegend* legFDB2 = new TLegend(0.13,0.37,0.5,0.88);
   legFDB2->SetLineColor(0);
-  legFDB2->SetTextSize(0.02);
-  legFDB2->SetNColumns(2);
+  legFDB2->SetTextSize(10);
+  //legFDB2->SetNColumns(2);
+
+  TLegend* legFDB4 = new TLegend(0.5,0.6,0.8,0.88);
+  legFDB4->SetLineColor(0);
+  legFDB4->SetTextSize(10);
+  //legFDB2->SetNColumns(2);
+
+  TLegend* legFDB3 = new TLegend(0.55,0.12,0.88,0.35);
+  legFDB3->SetLineColor(0);
+  legFDB3->SetTextSize(10);
+  //  legFDB3->SetNColumns(2);
 
   TGraphErrors* resolutionPlot[measurements];
   for (int i = 0 ; i< measurements ; i++){
@@ -82,7 +133,7 @@ void resolutionLiteratureSummary()
     
     resolutionPlot[i] = new TGraphErrors(1,pitch,res,pitchE,resE);
 
-    resolutionPlot[i]->SetMarkerSize(2.5);
+    resolutionPlot[i]->SetMarkerSize(1.5);
     resolutionPlot[i]->SetMarkerColor(colors[i]);
     resolutionPlot[i]->SetMarkerStyle(markers[i]);
 
@@ -91,8 +142,23 @@ void resolutionLiteratureSummary()
       resolutionPlot[i]->SetTitle(" ");
       resolutionPlot[i]->GetYaxis()->SetTitle("Resolution [#mum]");
       resolutionPlot[i]->GetXaxis()->SetTitle("Pitch [#mum]");
+      resolutionPlot[0]->GetXaxis()->SetTitleFont(43);
+      resolutionPlot[0]->GetXaxis()->SetTitleOffset(1.5);
+      resolutionPlot[0]->GetXaxis()->SetTitleSize(15); // labels will be 14 pixels
+
+      resolutionPlot[0]->GetYaxis()->SetTitleFont(43);
+      resolutionPlot[0]->GetYaxis()->SetTitleSize(15); // labels will be 14 pixels
+      resolutionPlot[0]->GetYaxis()->SetTitleOffset(1.8);
+
+
+      resolutionPlot[0]->GetXaxis()->SetLabelFont(43);
+      resolutionPlot[0]->GetXaxis()->SetLabelSize(15); // labels will be 14 pixels
+      resolutionPlot[0]->GetYaxis()->SetLabelFont(43);
+      resolutionPlot[0]->GetYaxis()->SetLabelSize(15); // labels will be 14 pixels
+      
+      
       resolutionPlot[i]->GetXaxis()->SetLimits(0.,160.);
-      resolutionPlot[i]->GetYaxis()->SetRangeUser(0.,55.);
+      resolutionPlot[i]->GetYaxis()->SetRangeUser(0.1,55.);
       resolutionPlot[i]->Draw("AEP");
     }
     else {
@@ -105,13 +171,22 @@ void resolutionLiteratureSummary()
       */
       resolutionPlot[i]->Draw("EPsame");      
     }
-    legFDB2->AddEntry(resolutionPlot[i],Legend[i],"p"); 
+    if (i <= 17) 
+      legFDB2->AddEntry(resolutionPlot[i],Label[i],"p");
+    else if(i > 17 && i < 25)
+      legFDB4->AddEntry(resolutionPlot[i],Label[i],"p");
+    else 
+      legFDB3->AddEntry(resolutionPlot[i],Label[i],"p");
+
+      //legFDB2->AddEntry(resolutionPlot[i],Legend[i],"p"); 
 
   }
 
-  
+  //cFDB2->SetLogy();
   legFDB2->Draw();
-  TF1* pi12 = new TF1("pi12","x/TMath::Sqrt(12)",0,150);
+  legFDB4->Draw();
+  legFDB3->Draw();
+  TF1* pi12 = new TF1("pi12","x/TMath::Sqrt(12)",0,160);
   pi12->Draw("lsame");
   
   TString  outname = outputDir+"ResolutionLiterature"; //+name;
